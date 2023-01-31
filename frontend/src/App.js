6b8ee5f3e503bdeef8d32e0edb32b79a120fadea
sirisha255 <112860662+sirisha255@users.noreply.github.com>
2023-01-31 12:58:56 +0100

    creating estore

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">EBUTIK</a>
+      </header>
+      <main>
+        List of Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
